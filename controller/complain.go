552ceeefeb304b0510dc7715115d34e0b095139d
Halim <oyurou.faiz@gmail.com>
2023-05-26 18:45:57 +0700

    Create Complaint without Upload File

---
+++ controller/complain.go
@@ -0,0 +1,28 @@
+package controller
+
+import (
+	"capstone/middleware"
+	"capstone/model/payload"
+	"capstone/usecase"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func CreateComplaintController(c echo.Context) error {
+	id, _ := middleware.ExtractTokenUserId(c)
+
+	req := &payload.CreateComplaintRequest{}
+	c.Bind(&req)
+
+	_, err := usecase.CreateComplaint(id, req)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, map[string]interface{}{
+			"message": "Failed To Create Complaint",
+			"error":   err.Error(),
+		})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "Success Created Complaint",
+	})
+}
