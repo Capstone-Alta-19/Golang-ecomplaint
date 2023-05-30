7be11f59ae75e8e076abe982ccae24b929043f1c
go_saniyyah-maharani <125669777+snymhranii@users.noreply.github.com>
2023-05-30 18:42:22 +0900

    ADD_Get Complaint

--- model/payload/Get_laporan&feedback.go
+++
@@ -1,7 +0,0 @@
-package payload
-
-type GetComplaintRequest struct {
-	Description string `json:"description" validate:"required"`
-	CategoryID  uint   `json:"category_id" validate:"required"`
-	Feedback    string `json:"feedback" validate:"required"`
-}
