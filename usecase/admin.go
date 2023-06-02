8627a1e134aac8ed841bd32a4a8286646bfac599
Halim <oyurou.faiz@gmail.com>
2023-06-02 16:39:56 +0700

    Update Logic Add Admin

--- usecase/admin.go
+++ usecase/admin.go
@@ -10,26 +10,29 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
 )
 
 func CreateAdmin(req payload.AddAdminRequest) (*model.Admin, error) {
 	_, err := database.GetAdminByUsername(req.Username)
-	if err == nil {
-		return nil, errors.New("username already used")
-	}
-
-	newAdmin := model.Admin{
-		Name:     req.Name,
-		Role:     req.Role,
-		Username: req.Username,
-		Password: req.Password,
-	}
-
-	err = database.CreateAdmin(&newAdmin)
 	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			newAdmin := model.Admin{
+				Name:     req.Name,
+				Role:     req.Role,
+				Username: req.Username,
+				Password: req.Password,
+			}
+			err := database.CreateAdmin(&newAdmin)
+			if err != nil {
+				return nil, err
+			}
+
+			return &newAdmin, nil
+		}
 		return nil, err
 	}
-	return &newAdmin, nil
+	return nil, errors.New("username already used")
 }
 
 func LoginAdmin(req payload.LoginAdminRequest) (*model.Admin, error) {
