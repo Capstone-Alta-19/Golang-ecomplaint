18c230962692d99b86705866a199a51b644d1153
go_khopipah-parawansah-siregar <khopipahparawansah@gmail.com>
2023-06-12 22:38:23 +0700

    hapus file

--- model/complaint.go
+++ model/complaint.go
@@ -1,19 +1,22 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type Complaint struct {
 	gorm.Model
-	UserID      uint     `json:"user_id"`
-	User        User     `json:"-"`
-	CategoryID  uint     `json:"category_id"`
-	Category    Category `json:"-"`
-	Type        string   `json:"type"`
-	Description string   `json:"description"`
-	PhotoURL    string   `json:"photo_url"`
-	VideoURL    string   `json:"video_url"`
-	IsPublic    bool     `json:"is_public"`
-	Feedback    string   `json:"feedback"`
+	UserID      uint      `json:"user_id"`
+	User        User      `json:"-"`
+	CategoryID  uint      `json:"category_id"`
+	Category    Category  `json:"-"`
+	Type        string    `json:"type"`
+	Description string    `json:"description"`
+	PhotoURL    string    `json:"photo_url"`
+	VideoURL    string    `json:"video_url"`
+	IsPublic    bool      `json:"is_public"`
+	Feedback    string    `json:"feedback"`
+	Comment     []Comment `json:"comment"`
 }
 
 type Feedback struct {
