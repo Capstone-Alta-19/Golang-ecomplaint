b1fcf277f1f29fc8972ea6111ce14e42053cd525
Halim <oyurou.faiz@gmail.com>
2023-06-13 13:27:21 +0700

    Update Model Like Relations

--- model/like.go
+++ model/like.go
@@ -4,6 +4,8 @@ import "gorm.io/gorm"
 
 type Like struct {
 	gorm.Model
-	UserID      uint `json:"user_id"`
-	ComplaintID uint `json:"complaint_id"`
+	UserID      uint      `json:"user_id"`
+	User        User      `json:"user"`
+	ComplaintID uint      `json:"complaint_id"`
+	Complaint   Complaint `json:"complaint"`
 }
