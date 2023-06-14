809726c14379862d54382312d6b1023254abdb28
Arif Mardhatillah Asmar <asmararif6@gmail.com>
2023-06-14 18:50:44 +0700

    Model User

--- model/user.go
+++ model/user.go
@@ -8,12 +8,13 @@ import (
 
 type User struct {
 	gorm.Model
-	Username  string    `json:"username"`
-	Email     string    `json:"email"`
-	FullName  string    `json:"full_name"`
-	Password  string    `json:"password"`
-	Phone     string    `json:"phone"`
-	DateBirth time.Time `json:"date_birth"`
-	Role      string    `json:"role"`
-	Token     string    `gorm:"-"`
+	PhotoProfile string    `json:"photo_profile"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	FullName     string    `json:"full_name"`
+	Password     string    `json:"password"`
+	Phone        string    `json:"phone"`
+	DateBirth    time.Time `json:"date_birth"`
+	Role         string    `json:"role"`
+	Token        string    `gorm:"-"`
 }
