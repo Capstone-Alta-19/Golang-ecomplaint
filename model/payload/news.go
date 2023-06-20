3de71a26781999c5540ee304fa0d0779bb58a1a7
Halim <oyurou.faiz@gmail.com>
2023-06-20 15:03:46 +0700

    Update payload AddNews

--- model/payload/news.go
+++ model/payload/news.go
@@ -2,6 +2,24 @@ package payload
 
 type CreateNews struct {
 	NewsName    string `json:"news_name" validate:"required"`
+	PhotoURL    string `json:"photo_url"`
 	Description string `json:"description" validate:"required"`
 	CategoryID  uint   `json:"category_id" validate:"required"`
+}
+
+type GetNewsByIDResponse struct {
+	ID          uint       `json:"id"`
+	NewsName    string     `json:"news_name"`
+	PhotoURL    string     `json:"photo_url"`
+	Description string     `json:"description"`
+	Admin       string     `json:"admin"`
+	Category    string     `json:"category"`
+	CreatedAt   string     `json:"created_at"`
+	NewsList    []NewsList `json:"news_list"`
+}
+
+type NewsList struct {
+	ID       uint   `json:"id"`
+	NewsName string `json:"news_name"`
+	PhotoURL string `json:"photo_url"`
 }
